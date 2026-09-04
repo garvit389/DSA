359a06d41511b00d730dde16775e4a1e717cd3ff
garvit389 <garvitsati07@gmail.com>
2026-09-04 14:35:38 +0530

    3_longest_substring_without_repeating_characters

---
+++ leetcode/3_longest_substring_without_repeating_characters.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        unordered_map<char, int> freq;
+        
+        int left = 0;
+        int maxL = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            
+            freq[s[right]]++;
+
+            while (freq[s[right]] > 1) {
+                freq[s[left]]--;
+                left++;
+            }
+
+            maxL = max(maxL, right - left + 1);
+        }
+
+        return maxL;
+    }
+};
+
